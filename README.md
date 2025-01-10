e910eb4e9bad0bb0f888d30150643f58140fb083
rihabcherni <rihabcherni000@gmail.com>
2025-01-11 00:46:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vayetek-Test-Technique
---
+++ test.py
@@ -0,0 +1,13 @@
+import re
+def calculer_somme_etalon(fichier):
+    s = 0
+    with open(fichier, 'r') as file:
+        for i, ligne in enumerate(file, start=1):
+            chiffres = re.findall(r'\d', ligne.strip())
+            premier = chiffres[0]
+            dernier = chiffres[-1]            
+            s +=  int(premier + dernier)
+    return s
+print("La somme totale des valeurs d'étalonnage est :", calculer_somme_etalon("document.txt"))
+
+# ====> La somme totale des valeurs d'étalonnage est : 53386
